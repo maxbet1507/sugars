08845d7351bd85d0f3b59f24f78df90790a93a97
maxbet <yoji.takahashi@gmail.com>
2017-08-28 22:30:20 +0900

    example修正

--- netsv/serve_test.go
+++ netsv/serve_test.go
@@ -2,31 +2,44 @@ package netsv
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"testing"
 	"time"
 )
 
-func TestServe(t *testing.T) {
+func Example() {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	defer cancel()
 
-	go func() {
-		conn, err := net.Dial("tcp", "127.0.0.1:10000")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer conn.Close()
-	}()
+	for i := 0; i < 3; i++ {
+		go func() {
+			conn, err := net.Dial("tcp", "127.0.0.1:10000")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer conn.Close()
+		}()
+	}
 
 	fn := func(ctx context.Context, conn net.Conn) {
+		fmt.Println("serving")
+
 		<-ctx.Done()
 		time.Sleep(100 * time.Millisecond)
 	}
 
 	if err := ServeFunc(ctx, "tcp", "127.0.0.1:10000", fn); err != nil {
-		t.Fatal(err)
+		fmt.Println(err)
 	}
+
+	fmt.Println("graceful")
+	// Output:
+	// serving
+	// serving
+	// serving
+	// graceful
 }
 
 func TestServeListenError(t *testing.T) {
